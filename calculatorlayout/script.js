ef0e1ebe181d03adb9fedd2a4ed695dc6e5b46e3
Anonmityslayer <nonmityslayer@tutanota.com>
2023-08-15 00:44:06 +0530

    day3 some calc stuff

---
+++ calculatorlayout/script.js
@@ -0,0 +1,12 @@
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+  button.addEventListener("click",(e)=>{
+    if(e.target.innerHTML )
+  })
+});
