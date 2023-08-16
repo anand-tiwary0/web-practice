2db6d5b1eb886654dedb9258dc0f953587bc462d
Anonmityslayer <nonmityslayer@tutanota.com>
2023-08-16 20:26:05 +0530

    dommanipulation

---
+++ dommanipulation/js.js
@@ -0,0 +1,6 @@
+const body = document.body;
+
+const newElement = document.createElement("a");
+document.appendChild("newElement");
+newElement.setAttribute("href", "google.com");
+newElement.innerText = "click here to open google.com";
