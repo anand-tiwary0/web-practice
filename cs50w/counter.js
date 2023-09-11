84917ba241fc84e6ba48bb88edf549509b55753f
Anonmityslayer <nonmityslayer@tutanota.com>
2023-09-11 23:53:20 +0530

    some webdev

--- cs50w/counter.js
+++ cs50w/counter.js
@@ -1,20 +1,20 @@
-if(!localStorage.getItem("counter")){
-    localStorage.setItem("counter",0);
+if (!localStorage.getItem("counter")) {
+  localStorage.setItem("counter", 0);
 }
-function count(){
-    let counter = localStorage.getItem("counter");
-    console.log(counter);
-    counter = parseInt(counter) + 1;
-    document.querySelector("#display").innerHTML = counter;
-    // if(counter%10 ===0){
-    //     alert(`counter is now ${counter}`)
-    // }
-    localStorage.setItem("counter",counter);
+function count() {
+  let counter = localStorage.getItem("counter");
+  console.log(counter);
+  counter = parseInt(counter) + 1;
+  document.querySelector("#display").innerHTML = counter;
+  // if(counter%10 ===0){
+  //     alert(`counter is now ${counter}`)
+  // }
+  localStorage.setItem("counter", counter);
 }
-document.addEventListener("DOMContentLoaded",function (){
-    document.querySelector("div").innerHTML = localStorage.getItem('counter');
-    document.querySelector("button").addEventListener("click",count);
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("div").innerHTML = localStorage.getItem("counter");
+  document.querySelector("button").addEventListener("click", count);
 
-    // setInterval(count,1000);
-})
+  // setInterval(count,1000);
+});
 // document.querySelector("button").onclick = count;
